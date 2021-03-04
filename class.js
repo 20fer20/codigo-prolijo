50adaab1655f01b89416c2b047819a37c4757d4c
20fer20 <72565453+20fer20@users.noreply.github.com>
2021-03-04 18:42:11 -0300

    Codigo corregido!

--- class.js
+++ class.js
@@ -1,20 +1,18 @@
-class Foo
-{
-color='negro';peso   =    34;
-  ver()
-  {
+class Foo {
+  color = 'negro';
+  peso = 34;
+  ver() {
     console.info(`Color: ${this.color}. Peso: ${this.peso}`
     );
-// foo = 123;
-/*
-// Prueba
-test() {
-  console.info('Test');
-}
-*/
-}
-   }    class Bar   {
-  miMetodo(){if(this.imprimir){console.info('Imprimir!');}}
+
+  }
+} class Bar {
+  miMetodo() {
+    if (this.imprimir) {
+      console.info('Imprimir!');
+      imprimir = true;
+    }
+  }
 }
 
 // Agregar property "imprimir=true" para que "miMetodo()" se ejecute.
